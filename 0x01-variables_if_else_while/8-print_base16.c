77a2364f98ede1e3f2aa5341998da61eecd3e8fc
boadu <boadu2g20@gmail.com>
2022-10-14 15:45:19 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - if while
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 16; n++)
+	{
+		if (n < 10)
+		{
+			putchar('0' + n);
+		}
+		else
+		{
+			putchar(87 + n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
