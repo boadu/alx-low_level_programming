04cee041336aa11f541f9e979fefaf6c8e4464d6
boadu <boadu2g20@gmail.com>
2022-10-18 07:19:20 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *abena = "_putchar"
+
+		while (*abena)
+		{
+			_putchar(*abena);
+			abena++;
+		}
 	_putchar('\n');
 
 	return (0);
