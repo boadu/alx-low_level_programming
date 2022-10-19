2b3840642fffbced8dc19f773a8513c698b5a042
boadu <boadu2g20@gmail.com>
2022-10-19 06:17:59 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - print 10 times.
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char s;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (s = 'a'; s <= 'z'; s++)
+		{
+			_putchar(s);
+		}
+		_putchar('\n');
+	}
+}
