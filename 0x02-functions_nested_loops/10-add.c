e7b719d6b08875e2cad7c18485db5da924ab6095
boadu <boadu2g20@gmail.com>
2022-10-19 10:08:31 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @h: the first number
+ * @e: the second number
+ * Return: the sum of two numbers
+ */
+int add(int h, int e)
+{
+	return (h + e);
+}
