e16a6df45d1ad2a27b1e1752af9b02a9b34dc27a
boadu <boadu2g20@gmail.com>
2022-10-20 07:04:14 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _issupper -> this is a function to check upppercase alphabets
+ * @c: an integer parameter
+ * Return: something but success
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
