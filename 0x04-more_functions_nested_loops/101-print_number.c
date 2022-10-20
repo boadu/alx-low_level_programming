fed9622841c0d59f69354745d4b4235c6e11c871
boadu <boadu2g20@gmail.com>
2022-10-20 08:56:11 -0700

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * prime_number - output a number
+ * @n: an int given from main
+ *
+ *
+ * Return: no value to return
+ */
+
+void print_number(int n)
+{
+	int x = n;
+	int digit;
+	int places = 1000000000;
+
+	if (n < 0)/* E */
+	{
+		x = -n;
+		_putchar('-');
+	}
+	if (n == INT_MAX || n == INT_MIN) /* D */
+	{
+		while (1)
+		{
+			if (n == INT_MAX)
+			{
+				_putchar('2');
+				_putchar('1');
+				_putchar('4');
+				_putchar('7');
+				_putchar('4');
+				_putchar('8');
+				_putchar('3');
+				_putchar('6');
+				_putchar('4');
+				_putchar('7');
+				break;
+			}
+			else if (n == INT_MIN)
+			{
+				_putchar('2');
+				_putchar('1');
+				_putchar('4');
+				_putchar('7');
+				_putchar('4');
+				_putchar('8');
+				_putchar('3');
+				_putchar('6');
+				_putchar('4');
+				_putchar('8');
+				break;
+			}
+		}
+	}
+	else if (n == 0)/* A */
+		_putchar('0');
+	else
+	{
+		while (places > x)/* B */
+			places /= 10;
+		while (places > 0)
+		{
+			digit = x / places;/* C */
+			_putchar((digit % 10) + '0');
+			places /= 10;
+		}
+	}
+}
